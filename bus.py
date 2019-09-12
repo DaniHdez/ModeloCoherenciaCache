a05b66cb45b73b9c30bdc81f9cb2c7a84d5a948e
DaniHdez <dannyha07@gmail.com>
2019-09-11 19:50:41 -0600

    Bus de memoria

--- bus.py
+++ bus.py
@@ -11,9 +11,38 @@
 #															#
 #############################################################
 
-import queue 
+import threading
 
-def busMemory(request)
+#a = procesador.getAddress (1, 4)
+#print (a)
+
+memRequests = []
+
+memory = [[0],[0],[0],[0],[0],[0],[0],[0],[0],[0],[0],[0],[0],[0],[0],[0]]
+
+def busMemory (idProcessor, address, action, data):
 #request = [idProcessor, address, act, miss]
+	memRequests.append([idProcessor, address, action, data])
+
+
+def controlBus (memRequests):
+	while True:
+		if (len(memRequests) != 0):
+			for request in memRequests:
+				if (request[2]=='write'):
+					memory[request[1]][0] = request[3]
+					print (memory)
+				elif (request[2]=='read'):
+					print (memory[request[1]][0])
+					#tempMemData = memory[address]
+					#Debo pasarlo de alguna forma al cache
+			memRequests.clear()
+			print ("\033[;31m"+" ", memory)
+
+
+#controlBus(memRequests)
+thread5 = threading.Thread(target = controlBus, args = (memRequests, ))
+thread5.start()			
+
 
 
